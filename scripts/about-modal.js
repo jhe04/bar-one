7e6090b7facb177314a010b9e161f8b619d28bd9
jhe04 <jielun.he@live.ca>
2022-05-13 22:08:13 -0400

    added pseudo code for script

---
+++ scripts/about-modal.js
@@ -0,0 +1,7 @@
+// add event listener to the "about" link in the nav bar
+//toggle hidden class
+
+// add event listener to the "close" button in the about-modal
+// toggle hidden class
+
+// add eventlistener to close the modal on pressing 'esc' or clicking outside of the modal window
